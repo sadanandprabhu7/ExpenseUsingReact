2ba0818e6c22bd27fb05708f48adb158c47d3603
sadanandprabhu7 <sadanandprabhu7@gmail.com>
2023-03-22 01:03:48 +0530

    items added

---
+++ src/Components/ExpenseItems.js
@@ -0,0 +1,14 @@
+function ExpenseItems() {
+  return (
+    <div>
+      <h2>Expense Items</h2>
+      <ol>
+        <li>Food Rs 10</li>
+        <li>Petrol Rs 100</li>
+        <li>Movies Rs 200</li>
+      </ol>
+    </div>
+  );
+}
+
+export default ExpenseItems;
