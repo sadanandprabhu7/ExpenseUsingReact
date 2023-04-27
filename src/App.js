05a24382df16da8f09d02398db0d32c4e24fe69b
sadanandprabhu7 <sadanandprabhu7@gmail.com>
2023-04-28 00:28:27 +0530

    added css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 //using node 14
-import ExpenseItems from "./Components/ExpenseItems";
+import React from "react";
+import Expenses from "./Components/Expenses/Expenses";
 function App() {
   const expense =[
-    {title:"item1",amount:10,date:new Date(),location:"lko"},
-    {title:"item1",amount:10,date:new Date(),location:"lko"},
-    {title:"item1",amount:10,date:new Date(),location:"lko"},
-    {title:"item1",amount:10,date:new Date(),location:"lko"}
+    {title:"item1",amount:10,date:new Date()},
+    {title:"item1",amount:10,date:new Date()},
+    {title:"item1",amount:10,date:new Date()},
+    {title:"item1",amount:10,date:new Date()}
   ]
-  const expenses = expense.map((ele)=>(
-    <ExpenseItems key={ele.title} title={ele.title} amount={ele.amount} date={ele.date} location={ele.location}></ExpenseItems>
-  ))
+
     return (
       <div>
         <h2>let's get started</h2>
-        {expenses}
+        <Expenses items={expense}></Expenses>
       </div>
     );
-
- 
-
 }
 
 export default App;
---
+++ src/Components/Expenses/ExpenseDate.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./ExpenseDate.css"
+function ExpenseDate (props)
+{
+    let month = props.date.toLocaleString("en-us",{month:"short"})
+    let day = props.date.toLocaleString("en-us",{day:"2-digit"})
+    let year = props.date.getFullYear()
+return (
+    <div className="expense-date ">
+        <div className="expense-date__day">{day}</div>
+        <div className="expense-date__month" >{month}</div>
+        <div className="expense-date__year">{year}</div>
+    </div>
+)
+}
+export default ExpenseDate
