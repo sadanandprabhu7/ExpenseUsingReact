7f65706406c94e951540d506a3949281429d107a
sadanandprabhu7 <sadanandprabhu7@gmail.com>
2023-05-01 03:28:25 +0530

    added message if only one left item

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React,{useState} from "react";
-import ExpenseItems from "./ExpenseItems";
 import Card from "../UI/Card";
 import "./Expenses.css"
 import ExpenseFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList";
 const Expenses=(props)=>
 {
   const [filteredYear,setFiltredYear]= useState("2023")
@@ -15,7 +15,7 @@ const Expenses=(props)=>
    return(
         <Card className="expenses">
           <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-          {filteredExpenses.map(expenseData=> (<ExpenseItems key={expenseData.id} title={expenseData.title} amount={expenseData.amount}date={expenseData.date}/>))}
+          <ExpensesList items={filteredExpenses}/>
       </Card>
     )
 } 
---
+++ src/Components/Expenses/ExpensesList.js
@@ -0,0 +1,34 @@
+import ExpenseItems from "./ExpenseItems";
+const ExpensesList = props => {
+    if(props.items.length === 0)
+    {
+        return <h2 className="expenses-list__fallback">Found no expenses</h2>
+    }
+    else if(props.items.length === 1)
+    {
+      return (
+        <div> 
+        <ul className="expense-list">
+        {props.items.map(expenseData=> (
+        <ExpenseItems key={expenseData.id}
+        title={expenseData.title} 
+        amount={expenseData.amount}
+        date={expenseData.date}/>))}
+        </ul>
+        <h2 className="expenses-list__fallback">Add more Expenses</h2>
+        </div>               
+        )
+    }
+    return (
+        <ul className="expense-list">
+        {props.items.map(expenseData=> (
+        <ExpenseItems key={expenseData.id}
+        title={expenseData.title} 
+        amount={expenseData.amount}
+        date={expenseData.date}/>))}
+        </ul>
+    )
+   
+}
+
+export default ExpensesList
